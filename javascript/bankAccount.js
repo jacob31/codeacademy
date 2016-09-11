b3cb358d71be97f4d19df14218e5b88f3d3173fd
jacob31 <bensilbernagel@gmail.com>
2016-09-11 13:24:09 -0700

    bank account game

---
+++ javascript/bankAccount.js
@@ -0,0 +1,25 @@
+/*
+filename: bankAccount.js
+author:   ben silbernagel
+created:  9-8-2016
+project:  simple bank account with password
+class:    codecademy
+*/
+
+function Person(first,last,age) {
+   this.firstname = first;
+   this.lastname = last;
+   this.age = age;
+   var bankBalance = 7500;
+  
+   this.askTeller = function(pass) {
+     if (pass == 1234) return bankBalance;
+     else return "Wrong password.";
+   };
+}
+
+var john = new Person('John','Smith',30);
+/* the variable myBalance should access askTeller()
+   with a password as an argument  */
+   myBalance = john.askTeller(1234);
+console.log(myBalance);
